af7eeeffe89a33e9d2be5d684f2585c684cf2b0d
Jaakko <jaheikela@gmail.com>
2017-07-09 13:32:17 +0300

    Save current work state.

--- src/main/java/com/training/train/ctrl/TrainCtrl.java
+++ src/main/java/com/training/train/ctrl/TrainCtrl.java
@@ -3,15 +3,24 @@ package com.training.train.ctrl;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.training.train.model.Train;
@@ -29,6 +38,18 @@ public class TrainCtrl {
 	@Bean
 	public UrlProvider urlProvider() {
 		return new UrlProvider();
+	}
+	
+	@ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE,
+			reason="Service temporarily unavailable. Please, try again later.")
+	@ExceptionHandler(RestClientException.class)
+	public void noServiceAvailable() {}
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+	public @ResponseBody TrainNotFoundMessage noTrainFound(HttpServletRequest req, Exception e) {
+		log.error(e.toString());
+		return new TrainNotFoundMessage(req.getRequestURL().toString(), e);
 	}
 	
 	@RequestMapping(path="/v1/train-numbers", method=RequestMethod.GET)
@@ -50,22 +71,31 @@ public class TrainCtrl {
 		}
 	}
 	
-	@RequestMapping(path="/v1/trains/{trainNumber}", method=RequestMethod.GET)
-	public Train getTrain(@PathVariable long trainNumber,
-			RestTemplate restTemplate, UrlProvider urlProvider) {
+	@RequestMapping(path="/v1/trains/{trainNumber}", method=RequestMethod.GET, produces="application/json")
+	public ResponseEntity<Train> getTrain(@PathVariable long trainNumber, RestTemplate restTemplate,
+			UrlProvider urlProvider, HttpServletResponse response) throws Exception {
 		NumberFormat format = NumberFormat.getIntegerInstance();
 		format.setGroupingUsed(false);
 		String numberStr = format.format(trainNumber);
 		String url = MessageFormat.format(urlProvider.getUrl("trainByNumberUrl"), numberStr);
 		log.info("url=" + url);
-		try {
-			Train[] trains = restTemplate.getForObject(url, Train[].class);
-			Train train = trains[0];
-			log.info(train.toString());
-			return train;
-		} catch(Exception e) {
-			log.error(e.toString());
-			return null;
-		}
+		Train[] trains = new Train[0];
+		trains = restTemplate.getForObject(url, Train[].class);
+//		} catch(Exception e) {
+//			log.error(e.toString());
+////			return e.toString();
+//			return new ResponseEntity<Train>(HttpStatus.SERVICE_UNAVAILABLE);
+//		}
+//		if (trains.length == 0) {
+//			String result = "No train was found with the given id.";
+//			log.info(result);
+//			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+////			return response.toString();
+//			return new ResponseEntity<Train>(HttpStatus.NOT_FOUND);
+//		}
+		Train train = trains[0];
+		log.info(train.toString());
+//		return train.toString();
+		return new ResponseEntity<Train>(train, HttpStatus.OK);
 	}
 }
